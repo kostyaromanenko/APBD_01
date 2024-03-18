02adc6336c62087f8d1c31ca26ecc1a8ab3cbe09
s28587 <s28587@pjwstk.edu.pl>
2024-03-19 00:37:51 +0100

    BranchTest1

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello World");
+﻿
+class Program
+{
+    public static double CalculateAverage(int[] numbers)
+    {
+        int sum = 0;
+        foreach (int number in numbers)
+        {
+            sum += number;
+        }
+        
+        return (double)sum / numbers.Length;
+    }
+
+    static void Main(string[] args)
+    {
+        int[] numbers = { 1, 2, 3, 4, 5 };
+        double average = CalculateAverage(numbers);
+        Console.WriteLine($"The average is: {average}");
+    }
+}
